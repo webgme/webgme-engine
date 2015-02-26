mchogan/webgme

tests run again (probably merge errors...)

src/common/core/core.js
@@ -15,8 +15,9 @@ define([
 'core/constraintcore',
 'core/coretree',
 'core/metacore',
+'core/corediff',
 'core/coretreeloader'],
-function (CoreRel, Set, Guid, NullPtr, UnWrap, Descriptor, Type, Constraint, CoreTree, MetaCore, TreeLoader)
+function (CoreRel, Set, Guid, NullPtr, UnWrap, Descriptor, Type, Constraint, CoreTree, MetaCore, Diff, TreeLoader)
 {
     "use strict";
 
@@ -24,7 +25,7 @@ function (CoreRel, Set, Guid, NullPtr, UnWrap, Descriptor, Type, Constraint, Cor
         options = options || {};
         options.usetype = options.usertype || 'nodejs';
 
-        var coreCon = new TreeLoader(new MetaCore(new Constraint(new Descriptor(new Guid(new Set(new NullPtr(new Type(new NullPtr(new CoreRel(new CoreTree(storage, options)))))))))));
+        var coreCon = new TreeLoader(new Diff(new MetaCore(new Constraint(new Descriptor(new Guid(new Set(new NullPtr(new Type(new NullPtr(new CoreRel(new CoreTree(storage, options))))))))))));
 
         if(options.usertype === 'tasync'){
             return coreCon;

webgme/webgme-engine

Conflicts:
	src/common/core/core.js
	src/common/core/setcore.js
Former-commit-id: b0267f672b7295ae930a6cc65c4c4ce674b20e53

src/common/core/core.js
@@ -25,7 +25,7 @@ define([
         options = options || {};
         options.usetype = options.usertype || 'nodejs';
 
-        var corecon = new CoreDiff(new TreeLoader(new MetaCore(new Constraint(new Descriptor(new Guid(new Set(new NullPtr(new Type(new NullPtr(new CoreRel(new CoreTree(storage, options))))))))))));
+        var corecon = new TreeLoader(new CoreDiff(new MetaCore(new Constraint(new Descriptor(new Guid(new Set(new NullPtr(new Type(new NullPtr(new CoreRel(new CoreTree(storage, options))))))))))));
 
         if(options.usertype === 'tasync'){
             return corecon;