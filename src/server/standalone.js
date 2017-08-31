92a52f949fb7e778cb1c1805c6300a1d44006e21
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2017-08-31 14:03:59 -0500

    Serve client from client directory.

--- src/server/standalone.js
+++ src/server/standalone.js
@@ -865,7 +865,7 @@ function StandAloneServer(gmeConfig) {
     logger.debug('creating basic static content related routing rules');
     //static contents
     //javascripts - core and transportation related files //TODO: remove config, middleware and bin
-    __app.get(/^\/(common|config|bin|middleware)\/.*\.js$/, Express.static(__baseDir, {index: false}));
+    __app.get(/^\/(common|client)\/.*\.js$/, Express.static(__baseDir, {index: false}));
     __app.get(/^\/(dist)\/.*\.(js|css|map)$/, Express.static(path.join(__baseDir, '..'), {index: false}));
 
     //TODO remove this part as this is only temporary!!!
