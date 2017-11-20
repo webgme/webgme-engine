ce2536d16bb11e43b6664138eb5a7b4a52a7e954
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2017-11-20 14:03:45 -0600

    WIP src/common/* linted

--- src/common/regexp.js
+++ src/common/regexp.js
@@ -10,12 +10,12 @@ define([], function () {
         DB_HASH = new RegExp('^#[0-9a-zA-Z_]{40}$'),
         BRANCH = new RegExp('^[0-9a-zA-Z_]*$'),
         TAG = new RegExp('^[0-9a-zA-Z_]*$'),
-        RAW_BRANCH = new RegExp('^\\*[0-9a-zA-Z_]*$'),// This is how it's stored in mongodb, i.e. with a prefixed *.
+        RAW_BRANCH = new RegExp('^\\*[0-9a-zA-Z_]*$'), // This is how it's stored in mongodb, i.e. with a prefixed *.
         PROJECT = new RegExp('^(?!system\\.)(?!_)[0-9a-zA-Z_+]*$'), // project name may not start with system. or _
-        DOCUMENT_KEY = new RegExp('^[^(\$|\_)\.][^\.]*$'),//based on the MongoDB requirements (no '.' and no leading $)
+        DOCUMENT_KEY = new RegExp('^[^($|_).][^.]*$'), //based on the MongoDB requirements (no '.' and no leading $)
         PROJECT_NAME = new RegExp('^[0-9a-zA-Z_]+$'),
 
-        INVALID_CSS_CHARS = new RegExp('[!"#$%&\'()\*\+,\./:;<=>\?@\[\\\]^`{\|}~ ]+', 'g'),
+        INVALID_CSS_CHARS = new RegExp('[!"#$%&\'()*+,./:;<=>?@[\\]^`{|}~ ]+', 'g'),
         HTML_ELEMENT = new RegExp('<[a-z][\\s\\S]*>', 'i'),
         GUID = new RegExp('[a-z0-9]{8}(-[a-z0-9]{4}){3}-[a-z0-9]{12}', 'i'),
         BLOB_HASH = new RegExp('^[0-9a-f]{40}$');
