eb27e60ec5150db9a422972a1179e3a0854e741d
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2018-06-11 11:30:12 -0500

    WIP Add --exit flag to mocha allowing it to finish

--- test/travis.sh
+++ test/travis.sh
@@ -12,7 +12,7 @@ elif [ "$TEST_BROWSER" == "true" ]
 then
   export COMMAND="node ./node_modules/karma/bin/karma start karma.conf.js --browsers Firefox --single-run"
 else
-  export COMMAND="node ./node_modules/mocha/bin/mocha -R dot --timeout 10000"
+  export COMMAND="node ./node_modules/mocha/bin/mocha -R dot --timeout 10000 --exit"
   if [ "$RECURSIVE" == "true" ]
   then
     export COMMAND="$COMMAND --recursive $TEST_FOLDER"
